cd30ffe9c6b7a947df680328359e8ce2ae8039c2
prestontewell <preston@tewellphoto.com>
2019-03-06 21:35:21 -0600

    add validation to name-presence, uniqueness true, price -presence true numericality > 0 and description - length 10-500

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, :price, presence: true
+  validates :name, uniqueness: true
+  validates :description, length: {in: 10..500}
+  validates :price, numericality: {greater_than: 0 }
 end
