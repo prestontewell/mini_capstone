660112bb4af9668cc6623402fb615156ae11537d
prestontewell <preston@tewellphoto.com>
2019-03-10 12:28:45 -0500

    add search for Product title

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,11 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    search_term = params[:search]
+    if search_term
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
+    else 
+      @products = Product.all
+    end
     render "index.json.jbuilder"
   end
 
@@ -23,7 +28,7 @@ class Api::ProductsController < ApplicationController
     else
     #   # if user does not enter correct data do not save and inform user
     # end
-      render 'errors.json.jbuilder' status: :unprocessible_entity
+      render 'errors.json.jbuilder', status: :unprocessible_entity
     end
   end
 
