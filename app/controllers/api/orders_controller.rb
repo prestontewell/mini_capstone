a342aeb18b34b07a4d07cf360b125a9d9ea28af3
prestontewell <preston@tewellphoto.com>
2019-03-14 20:23:43 -0500

    orders create action complete

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,12 +1,23 @@
 class Api::OrdersController < ApplicationController
 
   def create
+    # subtotal = quantity * price
+    product = Product.find_by(id:params[:product_id])
+    calculated_subtotal = params[:quantity].to_i * product.price
+    tax_rate = 0.09
+    calculated_tax = calculated_subtotal * tax_rate
+    calculated_total = calculated_subtotal + calculated_tax
+
     @order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
-      quanitity: params[:quanitity]
-
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
       )
+    @order.save
+    render 'show.json.jbuilder'
   end
-
 end
+    
---
+++ app/views/api/orders/order.json.jbuilder
@@ -0,0 +1,1 @@
+json.message "Your order has been completed."
